a0aa688cab163a84aeadb254e0f9e52ef20241f3
gabrielslima <gabrielfatec61@gmail.com>
2019-03-17 20:52:27 -0300

    Add the create user test

--- operations_test.py
+++ operations_test.py
@@ -3,8 +3,12 @@ categoriesMock = [
     {'title': "80's fps", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'},
     {'title': "80's adventure", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'},
     {'title': "80's music", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'}
-]
-
+    ]
+usersMock = [
+    {'name':'Marlo', 'email':'marlon@email.com', 'picture':'htts://photos.domain.profile/marlo'},
+    {'name':'Gerald', 'email':'gerald@email.com', 'picture':'htts://photos.domain.profile/marlo'},
+    {'name':'Whindersson Nunes', 'email':'whinderssonnunes@email.com', 'picture':'htts://photos.domain.profile/marlo'}
+    ]
 def shouldAddAnewCategory():
     try:
         for category in categoriesMock:
@@ -39,7 +43,19 @@ def shouldDeleteAcategory():
     except Exception as err:
         print('TEST 4 FAILED: shouldDeleteAcategory')
         print(err)
+def shouldCreateUsers():
+    try:
+        for user in usersMock:
+            operations.createUser(user['name'], user['email'], user['picture'])
+        print('TEST 5 FAILED: shouldCreateUsers')
+    except Exception as err:
+        print('TEST 5 FAILED: shouldCreateUsers')
+        print(err)
+
 # shouldAddAnewCategory()
 shouldListAllCategories()
 shouldUpdateCategory()
 shouldDeleteAcategory()
+shouldCreateUsers()
+shouldListUsers()
+shouldListAspecificUser()
