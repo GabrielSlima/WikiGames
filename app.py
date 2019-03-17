c68e55a6cfbe545a4dfe236e29e9a841ccbfbf55
gabrielslima <gabrielfatec61@gmail.com>
2019-03-16 23:20:50 -0300

    Commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 gamemock = {'id': 1,'name': 'dom', 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'}
 allCategoriesMock = [
@@ -8,7 +9,25 @@ allCategoriesMock = [
     {'id': 2 ,'title': "80's music", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'}
 ]
 
-gamesListMock = [{'title': 'titulo'},{'title': 'titulo'},{'title': 'titulo'}]
+gamesListMock = [
+    {
+        'id':1, 
+'title': 'Dom',
+'category':'80s fps', 
+'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'
+},
+{
+    'id':2,
+    'title': 'Counter Strike',
+    'category':'80s fps',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'
+    },
+{
+    'id':3,
+    'title': '007',
+    'category':'80s fps',
+    'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'
+    }]
 @app.route('/login')
 def login():
     return 'This page will let people login on our app'
@@ -20,24 +39,33 @@ def logout():
 @app.route('/')
 @app.route('/categories')
 def showCategories():
-    for i in allCategoriesMock:
-        print(i['title'])
-    return render_template('publiccategories.html', categories=allCategoriesMock, items=gamesListMock)
+    return render_template('categories.html', categories=allCategoriesMock, items=gamesListMock)
+    # return render_template('publiccategories.html', categories=allCategoriesMock, items=gamesListMock)
 
 @app.route('/genre/<string:categoryName>/items')
 def showCatalogItems(categoryName):
-    return render_template('publiccatalog.html', )
+    return render_template('catalog.html', items = gamesListMock )
+    # return render_template('publiccatalog.html', items = gamesListMock )
 
-@app.route('/genre/<string:genreName>/<string:itemName>')
-def showCatalogItem(genreName, itemName):
+@app.route('/genre/<string:genreName>/<int:itemId>')
+def showCatalogItem(genreName, itemId):
     return render_template('showItem.html', game = gamemock)
-@app.route('/catalog/<int:itemId>/edit')
+    # return render_template('publicShowItem.html', game = gamemock)
+    
+@app.route('/catalog/<int:itemId>/edit', methods=['GET', 'PUT'])
 def editCatalogItem(itemId):
-    return 'This page will let us edit the item: %s' % itemId
+    if request.method == 'PUT':
+        return 'Voce editou o item.'
+    if request.method == 'GET':
+        return render_template('editCatalogItem.html', item=gamemock, categories=allCategoriesMock)
 
-@app.route('/catalog/<int:itemId>/delete')
+@app.route('/catalog/<int:itemId>/delete', methods=['GET', 'POST'])
 def deleteCatalogItem(itemId):
-    return 'This page will let us delete the item: %s' % itemId
+    print(request.method)
+    if request.method == 'POST':
+        return 'Voce deletou o item.'
+    if request.method == 'GET':
+        return render_template('deleteCatalogItem.html', game=gamemock)
 
 if __name__ == '__main__':
     app.debug = True
