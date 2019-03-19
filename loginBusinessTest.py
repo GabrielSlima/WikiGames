610808ac26c967a632dbe1ce8bc7d04b5153f606
gabrielslima <gabrielfatec61@gmail.com>
2019-03-19 02:01:14 -0300

    Use loginBusiness as class too

--- loginBusinessTest.py
+++ loginBusinessTest.py
@@ -2,15 +2,15 @@ import loginBusiness
 import uuid
 import unittest
 
-class loginBusinessTests(unittest.TestCase):
+class loginBusinessTests():
     requests = ['GET', 'POST']
     tokenSessionAfterGetRequest = ''
-    loginBusinessClass = loginBusiness()
+
     def tokenValidationShouldBeTrue(self):
         tokenSessionAfterPostRequest = ''
         for request in self.requests:
             if request == 'POST':
-                if loginBusinessClass.validateUserSession(tokenSessionAfterGetRequest, tokenSessionAfterPostRequest):
+                if loginBusiness.LoginBusiness().validateUserSession(tokenSessionAfterGetRequest, tokenSessionAfterPostRequest):
                     print('TEST 1 SUCCESS: tokenValidationShouldBeTrue')
                 else:
                     print('TEST 1 FAILED: tokenValidationShouldBeTrue')
@@ -23,23 +23,24 @@ class loginBusinessTests(unittest.TestCase):
             if request == 'POST':
                 tokenSessionAfterPostRequest = str(uuid.uuid4())
             tokenSessionAfterGetRequest = str(uuid.uuid4())
-        if not loginBusiness.validateUserSession(tokenSessionAfterGetRequest, tokenSessionAfterPostRequest):
+        if not loginBusiness.LoginBusiness().validateUserSession(tokenSessionAfterGetRequest, tokenSessionAfterPostRequest):
             print('TEST 2 SUCCESS: tokenValidationShouldBeFalse')
         else:
             print('TEST 2 FAILED: tokenValidationShouldBeFalse')
 
     def checkIfUserWasAlreadyLogged(self):
         tokenSessionAfterGetRequest = str(uuid.uuid4())
-        if loginBusiness.checkIfUserWasAlreadyLogged(tokenSessionAfterGetRequest):
+        if loginBusiness.LoginBusiness().checkIfUserWasAlreadyLogged(tokenSessionAfterGetRequest):
             print('TEST 3 SUCCESS: checkIfUserWasAlreadyLogged')
         else:
             print('TEST 3 FAILED: checkIfUserWasAlreadyLogged')
        
     def shouldThrowExceptionWhenHasNoClientSecretsFile(self):
-        if not loginBusiness.checkIfFacebookClientSecretsExists():
+        if not loginBusiness.LoginBusiness().checkIfFacebookClientSecretsExists():
             print('TEST 4 SUCCESS: shouldThrowExceptionWhenHasNoClientSecretsFile')
         else:
             print('TEST 4 FAILED: shouldThrowExceptionWhenHasNoClientSecretsFile')
+
 
 tests = loginBusinessTests()
 tests.tokenValidationShouldBeTrue()
