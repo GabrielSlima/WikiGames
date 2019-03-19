17cb6246462780af150cf71023e435dd5b1387cb
gabrielslima <gabrielfatec61@gmail.com>
2019-03-19 02:53:41 -0300

    Add the validation of a non existing client_secrets file and add a method to retrieve a long term access token from facebook

--- loginBusiness.py
+++ loginBusiness.py
@@ -1,6 +1,9 @@
+import json
+import httplib2
 class LoginBusiness:
     FACEBOOK_CLIENT_ID = ''
-    FACEBOOK_CLIENT_SECRETS = ''
+    FACEBOOK_CLIENT_SECRET = ''
+    LONG_TERM_ACCESS_TOKEN= ''
 
     def validateUserSession(self, tokenSessionAfterGetRequest, tokenSessionAfterPostRequest):
         if tokenSessionAfterGetRequest == tokenSessionAfterPostRequest:
@@ -16,12 +19,35 @@ class LoginBusiness:
 
     def checkIfFacebookClientSecretsExists(self):
         try:
-            facebookSecrets = open('fb_ecrets.json','r').read()
+            facebookSecrets = open('fb_secrets.json','r').read()
+            self.setFacebookClientId(json.loads(facebookSecrets)['web']['client_id'])
+            self.setFacebookClientSecret(json.loads(facebookSecrets)['web']['client_secret'])
+            return True
         except FileNotFoundError:
             return False
 
+    def getLongTermAccessToken(self, app_id,app_secret,access_token):
+        url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
+        http = httplib2.Http()
+        header, content = http.request(url, 'GET')
+        if header['status'] != '200':
+            return False
+        self.setLongTermAccessToken(json.loads(content.decode('utf-8'))['access_token'])
+        return True
     def setFacebookClientId(self,client_id):
         self.FACEBOOK_CLIENT_ID = client_id
 
     def setFacebookClientSecret(self,client_secret):
-        self.FACEBOOK_CLIENT_SECRETS = client_secret()
+        self.FACEBOOK_CLIENT_SECRET = client_secret
+
+    def getFacebookClientId(self):
+        return self.FACEBOOK_CLIENT_ID
+
+    def getFacebookClientSecret(self):
+        return self.FACEBOOK_CLIENT_SECRET
+    
+    def setLongTermAccessToken(self, longTermAccessToken):
+        self.LONG_TERM_ACCESS_TOKEN = longTermAccessToken
+    
+    def getLongTermAccessTokenFromFacebook(self):
+        return self.LONG_TERM_ACCESS_TOKEN
