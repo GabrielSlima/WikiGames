1f284f17e0eebc8a79678d622c2e3c72f0b19d0d
gabrielslima <gabrielfatec61@gmail.com>
2019-03-23 15:51:45 -0300

    Implements the pep8

--- operations.py
+++ operations.py
@@ -2,19 +2,21 @@ from engine import engine
 from model import Game, User, Category
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-session = scoped_session(sessionmaker(bind = engine))
+session = scoped_session(sessionmaker(bind=engine))
+
 
 def deleteUser(user_id):
     try:
-        user = session.query(User).filter_by(id = user_id).one()
+        user = session.query(User).filter_by(id=user_id).one()
         session.delete(user)
         session.commit()
     except Exception as err:
-        return err    
+        return err
+
 
 def updateUser(user_id, user_name, user_email, user_picture):
     try:
-        newUser = session.query(User).filter_by(id = user_id).one()
+        newUser = session.query(User).filter_by(id=user_id).one()
         newUser.name = user_name
         newUser.email = user_email
         newUser.picture = user_picture
@@ -23,19 +25,22 @@ def updateUser(user_id, user_name, user_email, user_picture):
     except Exception as err:
         return err
 
+
 def getUserId(user_email):
     try:
-        user = session.query(User).filter_by(email = user_email).one()
+        user = session.query(User).filter_by(email=user_email).one()
         return user.id
     except:
         return None
 
+
 def getUser(user_id):
     try:
-        user = session.query(User).filter_by(id = user_id).one()
+        user = session.query(User).filter_by(id=user_id).one()
         return user
     except:
         return None
+
 
 def lisUsers():
     try:
@@ -44,34 +49,42 @@ def lisUsers():
     except:
         return None
 
+
 def createUser(user_name, user_email, user_picture):
     try:
-        user = User(name = user_name, email = user_email, picture = user_picture)
+        user = User(name=user_name, email=user_email, picture=user_picture)
         session.add(user)
         session.commit()
-        user = session.query(User).filter_by(email = user_email).one()
+        user = session.query(User).filter_by(email=user_email).one()
         return user.id
     except:
         return None
-        
+
+
 def getCategory(category_id):
     try:
-        category = session.query(Category).filter_by(id = category_id).one()
+        category = session.query(Category).filter_by(id=category_id).one()
         return category
     except:
         return None
 
+
 def deleteCategory(category_id):
     try:
-        category = session.query(Category).filter_by(id = category_id).one()
+        category = session.query(Category).filter_by(id=category_id).one()
         session.delete(category)
         session.commit()
     except Exception as err:
         return err
-        
-def updateCategory(category_id, category_title, category_descr, category_user_id):
+
+
+def updateCategory(
+        category_id,
+        category_title,
+        category_descr,
+        category_user_id):
     try:
-        newCategory = session.query(Category).filter_by(id = category_id).one()
+        newCategory = session.query(Category).filter_by(id=category_id).one()
         newCategory.title = category_title
         newCategory.description = category_descr
         newCategory.user_id = category_user_id
@@ -80,6 +93,7 @@ def updateCategory(category_id, category_title, category_descr, category_user_id
     except Exception as err:
         return err
 
+
 def listCategories():
     try:
         categories = session.query(Category).all()
@@ -87,34 +101,43 @@ def listCategories():
     except:
         return None
 
+
 def createCategory(category_title, category_descr, category_user_id):
     try:
-        category = Category(title = category_title, description=category_descr, user_id = category_user_id)
+        category = Category(
+            title=category_title,
+            description=category_descr,
+            user_id=category_user_id)
         session.add(category)
         session.commit()
     except Exception as err:
         return err
 
+
 def getGamesByCategory(id_category):
     try:
-        games = session.query(Game).filter_by(category_id = id_category)
+        games = session.query(Game).filter_by(category_id=id_category)
         return games
     except:
         return None
 
+
 def getGame(game_id):
     try:
-        game = session.query(Game).filter_by(id = game_id).one()
+        game = session.query(Game).filter_by(id=game_id).one()
         return game
     except:
         return None
 
+
 def getLastAddedGames():
     try:
-        games = session.query(Game, Category).filter(Game.category_id == Category.id).limit(10).all()
+        games = session.query(Game, Category).filter(
+            Game.category_id == Category.id).limit(10).all()
         return games
     except:
         return None
+
 
 def listGames():
     try:
@@ -123,15 +146,31 @@ def listGames():
     except:
         return None
 
-def createGame(game_title, game_short_descr, game_long_descr,game_category_id,game_user_id):
+
+def createGame(
+        game_title,
+        game_short_descr,
+        game_long_descr,
+        game_category_id,
+        game_user_id):
     try:
-        game = Game(title = game_title, short_description = game_short_descr, long_description = game_long_descr, category_id = game_category_id, user_id = game_user_id)
+        game = Game(
+            title=game_title, short_description=game_short_descr,
+            long_description=game_long_descr, category_id=game_category_id,
+            user_id=game_user_id)
         session.add(game)
-        session.commit()    
+        session.commit()
     except Exception as err:
         return err
 
-def editGame(game_id, game_title, game_short_descr, game_long_descr,game_category_id,game_user_id):
+
+def editGame(
+        game_id,
+        game_title,
+        game_short_descr,
+        game_long_descr,
+        game_category_id,
+        game_user_id):
     try:
         newGame = session.query(Game).filter_by(id=game_id).one()
         newGame.title = game_title
@@ -141,14 +180,15 @@ def editGame(game_id, game_title, game_short_descr, game_long_descr,game_categor
         newGame.user_id = game_user_id
         session.add(newGame)
         session.commit()
-        newGame = session.query(Game).filter_by(id = game_id).one()
+        newGame = session.query(Game).filter_by(id=game_id).one()
         return newGame
     except:
         return None
 
+
 def deleteGame(gameId):
     try:
-        game = session.query(Game).filter_by(id = gameId).one()
+        game = session.query(Game).filter_by(id=gameId).one()
         session.delete(game)
         session.commit()
     except Exception as err:
--- operations_test.py
+++ operations_test.py
@@ -1,74 +1,118 @@
 import operations
 import os
 categoriesMock = [
-    {'title': "80's fps", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'},
-    {'title': "80's adventure", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'},
-    {'title': "80's music", 'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla pretium, lorem nec mollis ullamcorper, magna nulla lobortis eros, quis feugiat orci ipsum ac lorem. Sed aliquam nisi at dolor volutpat pharetra. Nam tincidunt sagittis risus. Suspendisse egestas gravida faucibus. Mauris viverra tortor ut semper pretium. In tincidunt tellus sed tincidunt hendrerit. Phasellus sem velit, auctor sed erat in, egestas consectetur nisi. Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam eget, mollis elit. In at odio non nisl rhoncus scelerisque. Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo, consectetur in lorem sit amet, sollicitudin ullamcorper orci. Nam eget fermentum libero. Phasellus viverra nisi lobortis felis sodales, sed faucibus arcu molestie.'}
-    ]
+    {'title': "80's fps",
+        'description': 'Lorem ipsum dolor sit amet,' +
+        ' consectetur adipiscing elit. Nulla pretium,' +
+        ' lorem nec mollis ullamcorper, magna nulla lobortis eros,' +
+        ' quis feugiat orci ipsum ac lorem. Sed aliquam nisi at' +
+        ' dolor volutpat pharetra. Nam tincidunt sagittis risus.' +
+        ' Suspendisse egestas gravida faucibus. Mauris viverra tortor' +
+        ' ut semper pretium. In tincidunt tellus sed tincidunt hendrerit.' +
+        ' Phasellus sem velit, auctor sed erat in, egestas consectetur nisi.' +
+        ' Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam' +
+        ' eget, mollis elit. In at odio non nisl rhoncus scelerisque.' +
+        '  Vivamus at hendrerit mauris, non feugiat metus. Nunc velit leo,' +
+        ' consectetur in lorem sit amet, sollicitudin ullamcorper orci.' +
+        ' Nam eget fermentum libero. Phasellus viverra nisi lobortis felis' +
+        ' sodales, sed faucibus arcu molestie.'},
+    {'title': "80's ADVENTURE",
+        'description': 'Lorem ipsum dolor sit amet,' +
+        ' consectetur adipiscing elit. Nulla pretium,' +
+        ' lorem nec mollis ullamcorper, magna nulla lobortis eros,' +
+        ' quis feugiat orci ipsum ac lorem. Sed aliquam nisi at' +
+        ' dolor volutpat pharetra. Nam tincidunt sagittis risus.' +
+        ' Suspendisse egestas gravida faucibus. Mauris viverra tortor' +
+        ' ut semper pretium. In tincidunt tellus sed tincidunt hendrerit.' +
+        ' Phasellus sem velit, auctor sed erat in, egestas consectetur nisi.' +
+        ' Nunc at porttitor nulla. Integer nec lorem luctus, vestibulum diam' +
+        ' eget, mollis elit. In at odio non nisl rhoncus scelerisque.' +
+        ' at hendrerit mauris, non feugiat metus. Nunc velit leo,r' +
+        ' in lorem sit amet, sollicitudin ullamcorper orci. Nam eget Vivamus' +
+        ' fermentum libero. Phasellus viverra nisi lobortis felis consectetu' +
+        ' sodales, sed faucibus arcu molestie.'}]
 usersMock = [
-    {'name':'Marlo', 'email':'marlon@email.com', 'picture':'htts://photos.domain.profile/marlo'},
-    {'name':'Gerald', 'email':'gerald@email.com', 'picture':'htts://photos.domain.profile/marlo'},
-    {'name':'Whindersson Nunes', 'email':'whinderssonnunes@email.com', 'picture':'htts://photos.domain.profile/marlo'}
+    {
+        'name': 'Marlo',
+        'email': 'marlon@email.com',
+        'picture': 'htts://photos.domain.profile/marlo'
+        },
+    {
+        'name': 'Gerald',
+        'email': 'gerald@email.com',
+        'picture': 'htts://photos.domain.profile/marlo'
+        },
+    {
+        'name': 'Whindersson Nunes',
+        'email': 'whinderssonnunes@email.com',
+        'picture': 'htts://photos.domain.profile/marlo'
+        }
     ]
 gamesMock = [
-    {
-        'title':'Doom', 
-        'short_description':'The Doom franchise is a series of first-person'+ 
-    ' shooter video games developed by id Software, and related novels, comics, board games, and film adaptation.', 
-        'long_description': 'Doom is considered to be one of the ' + 
-    'pioneering first-person shooter games, introducing to IBM-compatible computers features' + 
-    ' such as 3D graphics, third-dimension spatiality, networked multiplayer gameplay, and support' + 
-    ' for player-created modifications with the Doom WAD format. Since the release of Doom in 1993,' + 
-    ' the series has spawned numerous sequels, expansion packs, and a film.Since its debut,' + 
-    ' over 10 million copies of games in the Doom series have been sold.',
-        'category_id': '000', 
-        'user_id':'000'
-        },
-    {
-        'title': 'Counter Strike',
-        'short_description':'Counter-Strike is a series of multiplayer' + 
-        ' first-person shooter video games, in which teams of terrorists' + 
-        ' battle to perpetrate an act of terror and counter-terrorists' + 
-        ' try to prevent it. The series began on Windows in 1999' + 
+    {'title': 'Doom',
+        'short_description': 'The Doom franchise is a series of first-person' +
+        ' shooter video games developed by id Software, and related novels,' +
+        ' comics, board games, and film adaptation.',
+        'long_description': 'Doom is considered to be one of the ' +
+        'pioneering first-person shooter games, introducing to' +
+        ' IBM-compatible computers features such as 3D graphics,' +
+        ' third-dimension spatiality, networked multiplayer gameplay,' +
+        ' and support for player-created modifications with' +
+        'the Doom WAD format. Since the release of Doom in 1993, the series' +
+        'has spawned numerous sequels, expansion packs, and a film.' +
+        'Since its debut, over 10 million copies of games in' +
+        'the Doom series have been sold.',
+        'category_id': '000',
+        'user_id': '000'},
+    {'title': 'Counter Strike',
+        'short_description': 'Counter-Strike is a series of multiplayer' +
+        ' first-person shooter video games, in which teams of terrorists' +
+        ' battle to perpetrate an act of terror and counter-terrorists' +
+        ' try to prevent it. The series began on Windows in 1999' +
         ' with the first game, Counter-Strike.',
-        'long_description': 'Counter-Strike (CS) is a series of multiplayer' + 
-        ' first-person shooter video games, in which teams of terrorists' + 
-        ' battle to perpetrate an act of terror (bombing, hostage-taking,' + 
-        ' assassination) and counter-terrorists try to prevent it' + 
-        ' (bomb defusal, hostage rescue). The series began on Windows in' + 
-        ' 1999 with the first game, Counter-Strike. It was initially' + 
-        ' released as a modification ("mod") for Half-Life and designed' + 
-        ' by Minh "Gooseman" Le and Jess "Cliffe" Cliffe before the rights' + 
-        ' to the game\'s intellectual property were acquired by' + 
-            ' Valve Corporation, the developers of Half-Life.',
-            'category_id': '000',
-            'user_id':'000'
-        },
-    {
-        'title': 'Zelda',
-    'short_description':'The Legend of Zelda is a fantasy action-adventure' + 
-    ' video game franchise created by Japanese game designers' + 
-    ' Shigeru Miyamoto and Takashi Tezuka. It is primarily developed ' + 
-    ' and published by Nintendo, although some portable installments' + 
-    ' and re-releases have been outsourced to Capcom, Vanpool, and Grezzo. ',
-    'long_description': 'The Legend of Zelda is a fantasy action-adventure' + 
-    ' video game franchise created by Japanese game designers' + 
-    ' Shigeru Miyamoto and Takashi Tezuka. It is primarily developed ' + 
-    ' and published by Nintendo, although some portable installments' + 
-    ' and re-releases have been outsourced to Capcom, Vanpool,' + 
-    ' and Grezzo. The series gameplay incorporates' + 
-    ' action-adventure and elements of action RPG games.',
-    'category_id': '000',
-    'user_id':'000'}
+        'long_description': 'Counter-Strike (CS) is a series of multiplayer' +
+        ' first-person shooter video games, in which teams of terrorists' +
+        ' battle to perpetrate an act of terror (bombing, hostage-taking,' +
+        ' assassination) and counter-terrorists try to prevent it' +
+        ' (bomb defusal, hostage rescue). The series began on Windows in' +
+        ' 1999 with the first game, Counter-Strike. It was initially' +
+        ' released as a modification ("mod") for Half-Life and designed' +
+        ' by Minh "Gooseman" Le and Jess "Cliffe" Cliffe before the rights' +
+        ' to the game\'s intellectual property were acquired by' +
+        ' Valve Corporation, the developers of Half-Life.',
+        'category_id': '000',
+        'user_id': '000'},
+    {'title': 'Zelda',
+        'short_description': 'The Legend of Zelda is a fantasy' +
+        ' action-adventure video game franchise created by Japanese games' +
+        '  designers Shigeru Miyamoto and Takashi Tezuka. It is primarily' +
+        ' developed and published by Nintendo, although some portable' +
+        ' installments and re-releases have been outsourced to Capcom,' +
+        ' Vanpool, and Grezzo. ',
+        'long_description': 'The Legend of Zelda is a action-adventure' +
+        ' video game franchise created by Japanese game designers' +
+        ' Shigeru Miyamoto and Takashi Tezuka. It is primarily developed ' +
+        ' and published by Nintendo, although some portable installments' +
+        ' and re-releases have been outsourced to Capcom, Vanpool,' +
+        ' and Grezzo. The series gameplay incorporates' +
+        ' action-adventure and elements of action RPG games.',
+        'category_id': '000',
+        'user_id': '000'}
     ]
+
+
 def shouldAddAnewCategory():
     try:
         for category in categoriesMock:
-            operations.createCategory(category['title'],category['description'], '00000')
+            operations.createCategory(
+                category['title'],
+                category['description'],
+                '00000')
         print('TEST 01 SUCCESS: shouldAddAnewCategory')
     except Exception as err:
         print('TEST 01 FAILED: shouldAddAnewCategory')
         print(err)
+
 
 def shouldListAllCategories():
     try:
@@ -78,25 +122,33 @@ def shouldListAllCategories():
         print('TEST 02 FAILED: shouldListAllCategories')
         print(err)
 
+
 def shouldUpdateCategory():
     try:
-        category = operations.listCategories()[len(operations.listCategories())-1]
+        category = operations.listCategories()[len(
+            operations.listCategories())-1]
         newCategory = categoriesMock[0]
-        operations.updateCategory(category.id, newCategory['title'], newCategory['description'], category.user_id)
-        print('TEST 03 SUCCESS: shouldUpdateCategory')        
+        operations.updateCategory(
+            category.id, newCategory['title'],
+            newCategory['description'],
+            category.user_id)
+        print('TEST 03 SUCCESS: shouldUpdateCategory')
     except Exception as err:
         print('TEST 03 FAILED: shouldUpdateCategory')
-        print(err) 
+        print(err)
+
 
 def shouldDeleteAcategory():
     try:
-        category = operations.listCategories()[len(operations.listCategories())-1]
+        category = operations.listCategories()[len(
+            operations.listCategories())-1]
         operations.deleteCategory(category.id)
-        print('TEST 04 SUCCESS: shouldDeleteAcategory')        
+        print('TEST 04 SUCCESS: shouldDeleteAcategory')
     except Exception as err:
         print('TEST 04 FAILED: shouldDeleteAcategory')
         print(err)
-        
+
+
 def shouldCreateUsers():
     try:
         for user in usersMock:
@@ -106,6 +158,7 @@ def shouldCreateUsers():
         print('TEST 05 FAILED: shouldCreateUsers')
         print(err)
 
+
 def shouldListUsers():
     try:
         operations.lisUsers()
@@ -113,6 +166,7 @@ def shouldListUsers():
     except Exception as err:
         print('TEST 06 FAILED: shouldListUsers')
         print(err)
+
 
 def shouldListAspecificUser():
     try:
@@ -123,6 +177,7 @@ def shouldListAspecificUser():
         print('TEST 07 FAILED: shouldListAspecificUser')
         print(err)
 
+
 def shouldEditAuser():
     try:
         user = operations.lisUsers()[len(operations.lisUsers())-1]
@@ -132,6 +187,7 @@ def shouldEditAuser():
         print('TEST 08 FAILED: shouldEditAuser')
         print(err)
 
+
 def shouldDeleteAuser():
     try:
         user = operations.lisUsers()[len(operations.lisUsers())-1]
@@ -140,21 +196,29 @@ def shouldDeleteAuser():
     except Exception as err:
         print('TEST 09 FAILED: shouldDeleteAuser')
         print(err)
-        
+
+
 def shouldCreateGames():
     try:
         user = operations.lisUsers()[len(operations.lisUsers())-1]
-        category = operations.listCategories()[len(operations.listCategories())-1]
+        category = operations.listCategories()[len(
+            operations.listCategories())-1]
         for game in gamesMock:
             game['user_id'] = user.id
             game['category_id'] = category.id
 
         for game in gamesMock:
-            operations.createGame(game['title'], game['short_description'], game['long_description'], game['category_id'], game['user_id'])
+            operations.createGame(
+                game['title'],
+                game['short_description'],
+                game['long_description'],
+                game['category_id'],
+                game['user_id'])
         print('TEST 10 SUCCESS: shouldCreateGames')
     except Exception as err:
         print('TEST 10 FAILED: shouldCreateGames')
         print(err)
+
 
 def shouldListGames():
     try:
@@ -163,6 +227,7 @@ def shouldListGames():
     except Exception as err:
         print('TEST 11 FAILED: shouldListGames')
         print(err)
+
 
 def shouldListAspecificGame():
     try:
@@ -173,22 +238,34 @@ def shouldListAspecificGame():
         print('TEST 12 FAILED: shouldListAspecificGame')
         print(err)
 
+
 def shouldListGamesOfAspecificCategory():
     try:
-        category = operations.listCategories()[len(operations.listCategories())-1]
+        category = operations.listCategories()[len(
+            operations.listCategories())-1]
         operations.getGamesByCategory(category.id)
-        print('TEST 13 SUCCESS: shouldLishouldListGamesOfAspecificCategorystAspecificGame')
+        print('TEST 13 SUCCESS: \
+        shouldLishouldListGamesOfAspecificCategorystAspecificGame')
     except Exception as err:
         print('TEST 13 FAILED: shouldListGamesOfAspecificCategory')
         print(err)
+
+
 def shouldEditAspecificGame():
     try:
         game = operations.listGames()[len(operations.listGames())-1]
-        operations.editGame(game.id,game.title,game.short_description,game.long_description, game.category_id, game.user_id)
+        operations.editGame(
+            game.id,
+            game.title,
+            game.short_description,
+            game.long_description,
+            game.category_id,
+            game.user_id)
         print('TEST 14 SUCCESS: shouldEditAspecificGame')
     except Exception as err:
         print('TEST 14 FAILED: shouldEditAspecificGame')
         print(err)
+
 
 def shouldDeleteAspecificGame():
     try:
@@ -199,6 +276,7 @@ def shouldDeleteAspecificGame():
         print('TEST 15 FAILED: shouldDeleteAspecificGame')
         print(err)
 
+
 def shouldGetUserIdByEmail():
     try:
         operations.lisUsers()[len(operations.lisUsers())-1]
@@ -206,6 +284,7 @@ def shouldGetUserIdByEmail():
     except Exception as err:
         print('TEST 16 FAILED: shouldGetUserIdByEmail')
         print(err)
+
 
 def shouldListLimitOfTenGames():
     try:
@@ -215,15 +294,17 @@ def shouldListLimitOfTenGames():
         print('TEST 17 FAILED: shouldListLimitOfTenGames')
         print(err)
 
+
 def shouldGetCategoryById():
     try:
-        category = operations.listCategories()[len(operations.listCategories())-1]
+        category = operations.listCategories()[len(
+            operations.listCategories())-1]
         operations.getCategory(category.id)
         print('TEST 18 SUCCESS: shouldGetCategoryById')
     except Exception as err:
         print('TEST 18 FAILED: shouldGetCategoryById')
         print(err)
-        
+
 shouldAddAnewCategory()
 shouldListAllCategories()
 shouldUpdateCategory()
